6a484ff38638e689137a9fd4af8a9b3b06dd5555
EddieFreedome <eddie.freed@tutanota.com>
2021-10-18 13:34:00 +0200

    impostato logica esercizio con commenti

---
+++ pali/script.js
@@ -0,0 +1,12 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+//prompt parola
+//creare un array delle lettere della parola
+//creare un array reverse della parola
+//confronto: il primo array e' uguale al secondo?
+//se si', e' palindroma, se no, non e' palindroma
+
+const parolaUtente = prompt("Inserisci una parola")
+
