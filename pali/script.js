bb3349e53b3b013dd5c620788f97fa8e7c637b56
EddieFreedome <eddie.freed@tutanota.com>
2021-10-18 14:43:08 +0200

    creata funzione palindroma

--- pali/script.js
+++ pali/script.js
@@ -10,12 +10,37 @@
 
 const parolaUtente = prompt("Inserisci una parola");
 console.log('ok');
-const arrParola = parolaUtente.split('');
-console.log(arrParola);
-const arrRevParola = arrParola.reverse();
-console.log(arrRevParola)
-if (parolaUtente === arrRevParola){
-    alert("La parola e' palindroma!")
-} else {
-    alert("La parola NON e' palindroma")
+
+    /*SENZA FUNZIONE*/
+// const arrParola = parolaUtente.split('');
+// console.log(arrParola);
+// const arrRevParola = arrParola.reverse();
+// console.log(arrRevParola)
+// if (parolaUtente === arrRevParola){
+//     alert("La parola e' palindroma!")
+// } else {
+//     alert("La parola NON e' palindroma")
+// }
+
+
+//e.s. funzione
+// function nomefunzione (argomento, argomento){}
+    //codice
+    //return valore
+// }
+
+/*FUNZIONE*/
+palindroma(parolaUtente)
+
+function palindroma(p) {
+    const arrParola = p.split('');
+    console.log(p)
+    console.log(arrParola);
+    const arrRevParola = arrParola.reverse().join('');
+    console.log(arrRevParola)
+    if (p === arrRevParola){
+        alert("La parola e' palindroma!")
+    } else {
+        alert("La parola NON e' palindroma")
+    }
 }
