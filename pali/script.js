e3478b51c4d69290318977107effe8830b802579
EddieFreedome <eddie.freed@tutanota.com>
2021-10-18 13:50:53 +0200

    added script without function

--- pali/script.js
+++ pali/script.js
@@ -8,5 +8,14 @@
 //confronto: il primo array e' uguale al secondo?
 //se si', e' palindroma, se no, non e' palindroma
 
-const parolaUtente = prompt("Inserisci una parola")
-
+const parolaUtente = prompt("Inserisci una parola");
+console.log('ok');
+const arrParola = parolaUtente.split('');
+console.log(arrParola);
+const arrRevParola = arrParola.reverse();
+console.log(arrRevParola)
+if (parolaUtente === arrRevParola){
+    alert("La parola e' palindroma!")
+} else {
+    alert("La parola NON e' palindroma")
+}
